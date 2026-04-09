90e318aa37f212b142b6b728e90dbea1d0f54156
DiegoGonz011 <diegovalenzuela@liceovvh.cl>
2026-04-09 12:58:17 -0400

    JS

---
+++ 01_Introduccion/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+//Prueba de conexion con js
+console.log("Hola mundo, desde archivo externo...");
+
+// 1) Hola mundo con variabñe y const.
+const Saludo="Hola a todos, ";
+let Destinatario="estudiante";
+//impresion de saludo concatenando con (+)
+console.log(Saludo + "para los " + Destinatario);
+
+// 2) Saludar a una persona ingresada por teclado
+let Nombre = prompt("Ingrese su nombre: ");
+//Ingresar un nombre ingresado por un prompt
+console.log("Hola, " + Nombre);
---
+++ 02_Tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("Conexion con js correcta")
+/*
+Datos: Tipo texto (string)
+Constenacion de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = "Diego";
+const apellido = "Gonzalez";
+//Unimos  ambas constantes con un texto extra
+console.log ("Hola, mi nombre es: " + nombre  + " " + apellido);
+
