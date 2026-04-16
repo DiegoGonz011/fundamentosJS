cdc518b9659154e6b16cda4b3f167938b633b2f6
DiegoGonz011 <diegovalenzuela@liceovvh.cl>
2026-04-16 12:57:10 -0400

    .

---
+++ 02_Tipos_de_datos/3.-tipo_numerico/static/script.js
@@ -0,0 +1,21 @@
+console.log("conexion exitosa con js")
+
+//tipos de datos numericos
+
+//1.-Declaracion de numeros (creacion de variables numericas)
+let edad = 31;
+let temperatura = -5;
+let precio = 19990;
+let pi = 3.1416;
+let descuento = 0.25;
+
+//operaciones basicas
+let suma = 10 + 5;
+let resta= 10 - 5;
+let multiplicacion = 10 * 5;
+let division = 10 / 2;
+let modulo = 10 % 3;
+let potencia = 2**3;
+
+console.log (`El resultado de 2 elevado a 3 es: ${2**3}`)
+
